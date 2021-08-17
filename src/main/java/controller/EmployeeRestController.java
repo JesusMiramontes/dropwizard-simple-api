6752135ccf884e98498463d4f71257af397370f0
Jesús Miramontes <4751856+JesusMiramontes@users.noreply.github.com>
2021-08-17 12:04:18 -0500

    Get all employees

--- src/main/java/controller/EmployeeRestController.java
+++ src/main/java/controller/EmployeeRestController.java
@@ -1,9 +1,14 @@
 package controller;
 
+import dao.EmployeeDao;
+import representations.Employee;
+
 import javax.validation.Validator;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/employees")
 @Produces(MediaType.APPLICATION_JSON)
@@ -13,5 +18,10 @@ public class EmployeeRestController {
 
     public EmployeeRestController(Validator validator) {
         this.validator = validator;
+    }
+
+    @GET
+    public Response getEmployees(){
+        return Response.ok(EmployeeDao.getEmployees()).build();
     }
 }
