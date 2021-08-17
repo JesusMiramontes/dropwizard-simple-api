5b4fa7bc50eabe6f6f89bbf04fff73869341eafd
Jesús Miramontes <4751856+JesusMiramontes@users.noreply.github.com>
2021-08-17 12:22:59 -0500

    Get employee by id

--- src/main/java/controller/EmployeeRestController.java
+++ src/main/java/controller/EmployeeRestController.java
@@ -6,6 +6,7 @@ import representations.Employee;
 import javax.validation.Validator;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -23,5 +24,15 @@ public class EmployeeRestController {
     @GET
     public Response getEmployees(){
         return Response.ok(EmployeeDao.getEmployees()).build();
+    }
+
+    @GET
+    @Path("/{id}")
+    public Response getEmployeeById(@PathParam("id") Integer id){
+        Employee employee = EmployeeDao.getEmployee(id);
+        if(employee != null)
+            return Response.ok(employee).build();
+        else
+            return Response.status(Response.Status.NOT_FOUND).build();
     }
 }
