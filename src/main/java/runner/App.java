83c37ef58bee7935c0367761801dcf2773b46e61
Jesús Miramontes <4751856+JesusMiramontes@users.noreply.github.com>
2021-08-17 11:11:10 -0500

    Extended from Application

--- src/main/java/runner/App.java
+++ src/main/java/runner/App.java
@@ -1,4 +1,19 @@
 package runner;
 
-public class App{
+import io.dropwizard.Application;
+import io.dropwizard.Configuration;
+import io.dropwizard.setup.Environment;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class App extends Application<Configuration> {
+    private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+
+    @Override
+    public void run(Configuration configuration, Environment environment) throws Exception {
+    }
+
+    public static void main(String[] args) throws Exception {
+        new App().run(args);
+    }
 }
