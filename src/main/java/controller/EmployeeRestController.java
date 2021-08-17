85cbc5e956fdccf2c14a0c5d2d2d88f7f2a90f54
Jesús Miramontes <4751856+JesusMiramontes@users.noreply.github.com>
2021-08-17 12:03:08 -0500

    Configured controller

--- src/main/java/controller/EmployeeRestController.java
+++ src/main/java/controller/EmployeeRestController.java
@@ -1,4 +1,17 @@
 package controller;
 
+import javax.validation.Validator;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/employees")
+@Produces(MediaType.APPLICATION_JSON)
 public class EmployeeRestController {
+
+    private final Validator validator;
+
+    public EmployeeRestController(Validator validator) {
+        this.validator = validator;
+    }
 }
